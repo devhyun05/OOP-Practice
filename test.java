5c4fe9645567619ef409bfdd8607b25a94400cf9
HyunSeongLee <devhyun05@gmail.com>
2023-12-16 23:14:41 -0500

    encapsulation

--- test.java
+++ test.java
@@ -1,9 +1,14 @@
 // 4 pillars of OOP
 
-// encapsulation
-class Tester {
+// encapsulation is something we hide the important part inside of the capsule 
+// display only some accessible functions that does not harm the program to users
 
-  private int x = 5;
-    
-  
+class TestClass {
+    // SIN number is critical
+    private int SIN; 
+
+    // we can only print the SIN, not allowed to modify 
+    public void printSIN() {
+        System.out.println("Print SIN: " + SIN); 
+    }
 }
