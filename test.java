939549c286d07262b355190edc88ad2d8a38a696
HyunSeongLee <devhyun05@gmail.com>
2023-12-17 22:36:44 -0500

    inheritance

--- test.java
+++ test.java
@@ -1,14 +1,11 @@
 // 4 pillars of OOP
 
-// encapsulation is something we hide the important part inside of the capsule 
-// display only some accessible functions that does not harm the program to users
+// inheritance
 
-class TestClass {
-    // SIN number is critical
-    private int SIN; 
+class Animal {
 
-    // we can only print the SIN, not allowed to modify 
-    public void printSIN() {
-        System.out.println("Print SIN: " + SIN); 
-    }
+}
+
+class Dog extends Animal {
+
 }
