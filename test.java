c4a8afcd77d0ece966d928c998deffff1a1396bb
HyunSeongLee <devhyun05@gmail.com>
2023-12-20 21:00:13 -0500

    polymorphism

--- test.java
+++ test.java
@@ -1,11 +1,16 @@
 // 4 pillars of OOP
 
-// inheritance
+// polymorphism
 
-class Animal {
-
+abstract class Animal {
+    void sound() {
+        System.out.println("Default sound");
+    }
 }
 
 class Dog extends Animal {
-
+    @Override
+    void sound() {
+        System.out.println("bark!"); 
+    }
 }
